7d887def8f0e91798b960198bfe26040b8560592
star-centauri <limacosta25@hotmail.com>
2020-07-29 09:59:08 -0300

    adiçao formulario categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
     return (
         <PageDefault>
-            Página de Cadastro de Categoria
+            <h1>Cadastro de Categoria</h1>
+
+            <form>
+
+                <label>
+                    Nome da Categoria:
+                    <input
+                    type="text"
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+
+            <Link to="/">
+            Ir para home
+            </Link>
         </PageDefault>
     )
 }
